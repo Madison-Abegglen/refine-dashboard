d278d9578f3bde0a752ba8c848fffbbedb69e6b5
madisonabegglen <madison.abegglen@gmail.com>
2024-03-27 14:03:39 -0500

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import { dataProvider, liveProvider } from "./providers/data";
+import { Home, ForgotPassword, Login, Register } from './pages';
+
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
@@ -37,8 +39,12 @@ function App() {
               >
                 <Routes>
                   <Route index element={<WelcomePage />} />
+                  <Route index element={<Home />} />
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
                 </Routes>
-                <RefineKbar />
+                <RefineKbar /> 
                 <UnsavedChangesNotifier />
                 <DocumentTitleHandler />
               </Refine>
