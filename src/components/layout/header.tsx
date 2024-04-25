72366d2fd8ecbef335d3c4e92a7d771ef13a9b99
madisonabegglen <madison.abegglen@gmail.com>
2024-04-25 12:57:25 -0500

    Complete Header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,25 @@
+import { Layout, Space } from "antd"
 import CurrentUser from "./current-user"
 
 const Header = () => {
+
+  const headerStyles: React.CSSProperties = {
+    background: '#fff',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    padding: '0 24px',
+    position: 'sticky',
+    top: 0,
+    zIndex: 999
+  }
+
   return (
-    <div>
+    <Layout.Header style={headerStyles}>
+      <Space align='center' size='middle'>
         <CurrentUser />
-    </div>
+      </Space>
+    </Layout.Header>
   )
 }
 
