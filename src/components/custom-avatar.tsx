fbcd06244ecbf62e3bdb2579542261f8e2d5fe15
madisonabegglen <madison.abegglen@gmail.com>
2024-04-25 09:32:05 -0500

    Update App Route & Layout Setup

---
+++ src/components/custom-avatar.tsx
@@ -0,0 +1,24 @@
+import { Avatar as AntdAvatar, AvatarProps } from "antd";
+
+type Props = AvatarProps & {
+    name: string;
+}
+
+const CustomAvatar = ({ name, style, ...rest }: Props) => {
+  return (
+    <AntdAvatar
+      alt="{Madison Abegglen}"
+      size="small"
+      style={{
+        backgroundColor: "#87d068",
+        display: "flex",
+        alignItems: "center",
+        border: "none",
+      }}
+    >
+      MA
+    </AntdAvatar>
+  );
+};
+
+export default CustomAvatar;
