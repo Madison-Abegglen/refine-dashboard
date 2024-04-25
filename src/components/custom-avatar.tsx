8ea41186e151688b256752a0c3744f19239d591c
madisonabegglen <madison.abegglen@gmail.com>
2024-04-25 12:47:55 -0500

    Add Utils, Current User Update, Account Settings

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,22 +1,25 @@
+import { getNameInitials } from "@/utilities";
 import { Avatar as AntdAvatar, AvatarProps } from "antd";
 
 type Props = AvatarProps & {
-    name: string;
+    name?: string;
 }
 
 const CustomAvatar = ({ name, style, ...rest }: Props) => {
   return (
     <AntdAvatar
-      alt="{Madison Abegglen}"
+      alt={name}
       size="small"
       style={{
         backgroundColor: "#87d068",
         display: "flex",
         alignItems: "center",
         border: "none",
+        ...style
       }}
+      {...rest}
     >
-      MA
+      {getNameInitials(name || '')}
     </AntdAvatar>
   );
 };
