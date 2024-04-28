03bb39c54c28aa3846ee682d8d963025e359e2be
madisonabegglen <madison.abegglen@gmail.com>
2024-04-28 12:50:08 -0500

    Adding companies, tasks resources

--- src/config/resources.tsx
+++ src/config/resources.tsx
@@ -1,4 +1,4 @@
-import { DashboardOutlined } from "@ant-design/icons";
+import { DashboardOutlined, ProjectOutlined, ShopOutlined } from "@ant-design/icons";
 import { IResourceItem } from "@refinedev/core";
 
 /**
@@ -20,6 +20,27 @@ export const resources: IResourceItem[] = [
         meta: {
             label: 'Dashboard',
             icon: <DashboardOutlined />
+        }
+    },
+    {
+        name: 'companies',
+        list: '/companies',
+        show: '/companies/:id',
+        create: '/companies/new',
+        edit: '/companies/edit/:id',
+        meta: {
+            label: 'Companies',
+            icon: <ShopOutlined />
+        }
+    },
+    {
+        name: 'tasks',
+        list: '/tasks',
+        create: '/tasks/new',
+        edit: '/tasks/edit/:id',
+        meta: {
+            label: 'Tasks',
+            icon: <ProjectOutlined />
         }
     }
 ]
