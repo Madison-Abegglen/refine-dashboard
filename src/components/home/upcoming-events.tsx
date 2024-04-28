3d2c5572e6820b8e0098038c5e03c492e69ccb50
madisonabegglen <madison.abegglen@gmail.com>
2024-04-28 14:03:41 -0500

    Skeletons & upcoming events - list wireframe

---
+++ src/components/home/upcoming-events.tsx
@@ -0,0 +1,62 @@
+import { CalendarOutlined } from "@ant-design/icons";
+import { Badge, Card, List } from "antd";
+import { Text } from "../text";
+import { useState } from "react";
+import UpcomingEventsSkeleton from "../skeleton/upcoming-events";
+import { getDate } from "@/utilities/helpers";
+
+const UpcomingEvents = () => {
+  const [isLoading, setisLoading] = useState(false);
+
+  return (
+    <Card
+      style={{ height: "100%" }}
+      headStyle={{ padding: "8px 16px" }}
+      bodyStyle={{ padding: "0 1rem" }}
+      title={
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
+          <CalendarOutlined />
+          <Text size="sm" style={{ marginLeft: "0.7rem" }}>
+            Upcoming Events
+          </Text>
+        </div>
+      }
+    >
+      {isLoading ? (
+        <List
+          itemLayout="horizontal"
+          dataSource={Array.from({ length: 5 }).map((_, index) => ({
+            id: index,
+          }))}
+          renderItem={() => <UpcomingEventsSkeleton />}
+        />
+      ) : (
+        <List
+          itemLayout="horizontal"
+          dataSource={[]}
+          renderItem={(item) => {
+            const renderDate = getDate(item.startDate, item.endDate)
+
+            return (
+              <List.Item>
+                <List.Item.Meta 
+                    avatar={<Badge color={item.color} />}
+                    title={<Text size="xs">{renderDate}</Text>}
+                    description={<Text ellipsis={{ tooltip: true }} strong>{item.Title}</Text>}
+                />
+              </List.Item>
+            );
+          }}
+        ></List>
+      )}
+    </Card>
+  );
+};
+
+export default UpcomingEvents;
