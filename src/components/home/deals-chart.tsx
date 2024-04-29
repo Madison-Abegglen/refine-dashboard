ddd12afe64105ce8d016f8c42cd6690670232aea
madisonabegglen <madison.abegglen@gmail.com>
2024-04-29 10:09:26 -0500

    Create Deals-Chart card & implement Area from AntDesign

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,8 +1,36 @@
+import { DollarOutlined } from '@ant-design/icons'
+import { Card } from 'antd'
 import React from 'react'
+import { Text } from '../text'
+import { Area, AreaConfig } from '@ant-design/plots'
 
 const DealsChart = () => {
+  const config: AreaConfig = {
+    data: []
+  }
+
   return (
-    <div>DealsChart</div>
+    <Card
+      style={{ height: '100%' }}
+      headStyle={{ padding: '8px 16px' }}
+      bodyStyle={{ padding: '24px 24px 0 24px' }}
+      title={
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px'
+          }}
+        >
+          <DollarOutlined />
+          <Text size='sm' style={{ marginLeft: '0.5rem' }}>
+            Deals
+          </Text>
+        </div>
+      }
+    >
+      <Area {...config} height={325} />
+    </Card>
   )
 }
 
