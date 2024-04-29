ab32e274826c4d312805cae19e8ae89c5e6271e9
madisonabegglen <madison.abegglen@gmail.com>
2024-04-29 11:12:32 -0500

    Complete Deals Chart

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -12,6 +12,11 @@ import { DashboardDealsChartQuery } from '@/graphql/types'
 const DealsChart = () => {
   const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
     resource: 'dealStages',
+    filters: [
+      {
+        field: 'title', operator: 'in', value: ['WON', 'LOST']
+      }
+    ],
     meta: {
       gqlQuery: DASHBOARD_DEALS_CHART_QUERY
     }
